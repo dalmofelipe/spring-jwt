ae8b0ea70161268f3c14bb9528a6ef939aa1a269
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-12-03 20:41:46 -0300

    adiciona user repository e login dto

---
+++ src/main/java/com/dalmofelipe/SpringJWT/User/LoginDTO.java
@@ -0,0 +1,23 @@
+package com.dalmofelipe.SpringJWT.User;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthToken() {
+        return new UsernamePasswordAuthenticationToken(password, email);
+    }
+
+}
---
+++ src/main/java/com/dalmofelipe/SpringJWT/User/UserRepository.java
@@ -0,0 +1,14 @@
+package com.dalmofelipe.SpringJWT.User;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByEmail();
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.id = :id")
+    Optional<User> findByIdWithJoinFecth(Long id);
+}
