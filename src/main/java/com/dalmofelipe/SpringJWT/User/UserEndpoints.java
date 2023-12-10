18e6e8acc3d4240a180200180233b8af897ecfca
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-12-10 13:42:19 -0300

    mend

--- src/main/java/com/dalmofelipe/SpringJWT/User/UserEndpoints.java
+++ src/main/java/com/dalmofelipe/SpringJWT/User/UserEndpoints.java
@@ -1,21 +1,13 @@
 package com.dalmofelipe.SpringJWT.User;
 
-import java.util.List;
-
+import com.dalmofelipe.SpringJWT.Exceptions.ApiError;
+import com.dalmofelipe.SpringJWT.Role.RoleRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.dalmofelipe.SpringJWT.Auth.dtos.RegisterDTO;
-import com.dalmofelipe.SpringJWT.Exceptions.ApiError;
-import com.dalmofelipe.SpringJWT.Role.RoleRecord;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
